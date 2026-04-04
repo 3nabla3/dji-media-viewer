f34ad121e05e88f7cabf219fcfb579e78f531e16
Alban Guyon <albanguyon@yahoo.fr>
2026-04-04 01:28:18 -0400

    refactor: use react-bootstrap Card/Badge/Row/Col in card components

--- components/cards/PanoramaCard.tsx
+++ components/cards/PanoramaCard.tsx
@@ -1,42 +1,63 @@
 // components/cards/PanoramaCard.tsx
-'use client'
+"use client";
 
-import type { PanoramaItem } from '@/lib/media-types'
-import { useThumbnail } from '@/lib/use-thumbnail'
+import { Card, Badge, Row, Col } from "react-bootstrap";
+import type { PanoramaItem } from "@/lib/media-types";
+import { useThumbnail } from "@/lib/use-thumbnail";
 
 function TileThumb({ file }: { file: File }) {
   console.log(file);
-  const { url } = useThumbnail(file)
+  const { url } = useThumbnail(file);
   return url ? (
     // eslint-disable-next-line @next/next/no-img-element
-    <img src={url} alt="" className="img-fluid" style={{ height: '60px', objectFit: 'cover', width: '100%' }} />
+    <img
+      src={url}
+      alt=""
+      className="img-fluid"
+      style={{ height: "60px", objectFit: "cover", width: "100%" }}
+    />
   ) : (
-    <div className="bg-secondary-subtle" style={{ height: '60px', width: '100%' }} />
-  )
+    <div
+      className="bg-secondary-subtle"
+      style={{ height: "60px", width: "100%" }}
+    />
+  );
 }
 
-export default function PanoramaCard({ item, onClick }: { item: PanoramaItem; onClick: () => void }) {
-  // Use the first tile's ref to drive lazy loading for the whole card
+export default function PanoramaCard({
+  item,
+  onClick,
+}: {
+  item: PanoramaItem;
+  onClick: () => void;
+}) {
   console.log(item);
-  const { ref } = useThumbnail(item.tiles[0])
+  const { ref } = useThumbnail(item.tiles[0]);
 
   return (
-    <div ref={ref} className="card h-100" style={{ cursor: 'pointer' }} onClick={onClick}>
-      <div className="card-header p-2">
-        <span className="badge bg-info text-dark me-1">PANORAMA</span>
+    <Card
+      ref={ref}
+      className="h-100"
+      style={{ cursor: "pointer" }}
+      onClick={onClick}
+    >
+      <Card.Header className="p-2">
+        <Badge bg="info" text="dark" className="me-1">
+          PANORAMA
+        </Badge>
         <small className="text-muted">
           {item.htmlFile.name} · {item.tiles.length} tiles
         </small>
-      </div>
-      <div className="card-body p-2">
-        <div className="row row-cols-4 g-1">
+      </Card.Header>
+      <Card.Body className="p-2">
+        <Row xs={4} className="g-1">
           {item.tiles.map((tile, i) => (
-            <div key={i} className="col">
+            <Col key={i}>
               <TileThumb file={tile} />
-            </div>
+            </Col>
           ))}
-        </div>
-      </div>
-    </div>
-  )
+        </Row>
+      </Card.Body>
+    </Card>
+  );
 }
--- components/cards/PhotoCard.tsx
+++ components/cards/PhotoCard.tsx
@@ -1,29 +1,46 @@
 // components/cards/PhotoCard.tsx
-'use client'
+"use client";
 
-import type { PhotoItem } from '@/lib/media-types'
-import { useThumbnail } from '@/lib/use-thumbnail'
+import { Card, Badge } from "react-bootstrap";
+import type { PhotoItem } from "@/lib/media-types";
+import { useThumbnail } from "@/lib/use-thumbnail";
 
-export default function PhotoCard({ item, onClick }: { item: PhotoItem; onClick: () => void }) {
-  const { url, ref } = useThumbnail(item.file)
+export default function PhotoCard({
+  item,
+  onClick,
+}: {
+  item: PhotoItem;
+  onClick: () => void;
+}) {
+  const { url, ref } = useThumbnail(item.file);
 
   return (
-    <div ref={ref} className="card h-100" style={{ cursor: 'pointer' }} onClick={onClick}>
+    <Card
+      ref={ref}
+      className="h-100"
+      style={{ cursor: "pointer" }}
+      onClick={onClick}
+    >
       {url ? (
         // eslint-disable-next-line @next/next/no-img-element
         <img
           src={url}
           alt={item.file.name}
           className="card-img-top"
-          style={{ height: '200px', objectFit: 'cover' }}
+          style={{ height: "200px", objectFit: "cover" }}
         />
       ) : (
-        <div className="card-img-top bg-secondary-subtle" style={{ height: '200px' }} />
+        <div
+          className="card-img-top bg-secondary-subtle"
+          style={{ height: "200px" }}
+        />
       )}
-      <div className="card-body p-2">
-        <span className="badge bg-success me-1">PHOTO</span>
+      <Card.Body className="p-2">
+        <Badge bg="success" className="me-1">
+          PHOTO
+        </Badge>
         <small className="text-muted">{item.file.name}</small>
-      </div>
-    </div>
-  )
+      </Card.Body>
+    </Card>
+  );
 }
